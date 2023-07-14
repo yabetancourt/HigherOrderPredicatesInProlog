0e8040f3662809b5adc982a2c1fec740d76859ba
yabetancourt <yabetancourt@uclv.cu>
2023-07-14 12:19:21 -0400

    Added reduce docs

--- prolog/reduce.pl
+++ prolog/reduce.pl
@@ -1,6 +1,10 @@
 :- module(reduce,[reduce/4]).
 
-% Predicado para aplicar una función a los elementos de una lista de manera acumulativa
+% reduce(F, L, Acc, Result) es un procedimiento que toma cuatro argumentos:
+% F, que es un predicado de tres argumentos;
+% L, que es una lista de elementos;
+% Acc, que es un acumulador inicial;
+% Result, que es el resultado final de aplicar el predicado F a los elementos de la lista.
 reduce(_, [], Acc, Acc).
 
 reduce(F, [H|T], Acc, Result) :-
