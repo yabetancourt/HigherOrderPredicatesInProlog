721a0689144b6f9e2578ca0833b8a27e1fa84fba
yabetancourt <yabetancourt@uclv.cu>
2023-07-16 10:14:28 -0400

    added partition procedure and examples

---
+++ examples/partition_examples.pl
@@ -0,0 +1,10 @@
+:- use_module('prolog/partition').
+
+es_par(N) :- N mod 2 =:= 0.
+
+separar_pares_impares(Lista, Pares, Impares) :-
+    partition(es_par, Lista, Pares, Impares).
+
+%?- separar_pares_impares([1,2,3,4,2,3,1], P, I).
+%P = [2, 4, 2],
+%I = [1, 3, 3, 1] .
---
+++ prolog/partition.pl
@@ -0,0 +1,17 @@
+:- module(partition,[partition/4]).
+
+% El predicado partition toma cuatro argumentos:
+% Obj, una función de un argumento que se utilizará para particionar la lista,
+% R1, una lista de elementos,
+% R2, donde se guarda el resultado.
+% R3, donde se guarda el resultado.
+% El predicado divide los elementos de R1 en dos listas, R2 y R3,
+% de tal manera que todos los elementos que satisfacen el predicado Obj se colocan en R2 y los que no lo satisfacen se colocan en R3.
+partition(_, [], [], []).
+
+partition(Obj, [X | R1], [X | R2], L) :-
+    call(Obj, X), !,
+    partition(Obj, R1, R2, L).
+
+partition(Obj, [X | R1], L, [X | R2]) :-
+    partition(Obj, R1, L, R2).
